cb4295f929ebaefa2d080c9dad7955dc5f955a07
Pedro Ferreira <pedro.ferreira@egypteam.com>
2024-12-22 06:14:06 -0300

    Update

---
+++ scripts/yaml-exec.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+THE_BASH_SOURCE=$BASH_SOURCE
+READ_LINK=$(readlink $THE_BASH_SOURCE)
+if [ "$READ_LINK" != "" ]; then
+    THE_BASH_SOURCE=$READ_LINK
+fi
+
+if [ "$2" == "" ]; then
+    SCOPE=default
+    SERVICE=$1
+else
+    SCOPE=$1
+    SERVICE=$2
+fi
+
+BINDIR=$(dirname $THE_BASH_SOURCE)
+BASEDIR=$(dirname $BINDIR)
+SCRIPTSDIR="$BASEDIR/scripts"
+DESCRIPTORSDIR="$BASEDIR/descriptors/$SCOPE"
+
+PODNAME=$(kubectl get pods | head -2 | tail -1 | tr -s " " | cut -f1 -d" " | grep -E "^$SERVICE\\.*")
+
+echo $PODNAME
+
+if [ "$PODNAME" != "" ]; then
+
+    kubectl exec -it $PODNAME /bin/bash
+
+fi
