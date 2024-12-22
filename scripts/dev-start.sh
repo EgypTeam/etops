7c8e58f6cc98db75159db93e7d09681719956b2e
Pedro Ferreira <pedro.ferreira@egypteam.com>
2024-12-22 14:51:03 -0300

    Update

--- scripts/dev-start.sh
+++ scripts/dev-start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-egt minikube start
+if [ "$1" == "" ]; then
+    PROVIDER=minikube
+else
+    PROVIDER=$1
+fi
+
+egt dev start-$PROVIDER
--- scripts/dev-stop.sh
+++ scripts/dev-stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-egt minikube stop
+if [ "$1" == "" ]; then
+    PROVIDER=minikube
+else
+    PROVIDER=$1
+fi
+
+egt dev stop-$PROVIDER
