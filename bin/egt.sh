eeca1ac021207c00f3ba2f5047a3893c74f90f91
Pedro Ferreira <pedro.ferreira@egypteam.com>
2024-12-22 05:06:35 -0300

    Update

--- bin/egt.sh
+++ bin/egt.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ "$1" == "" ]; then
+    echo "Missing command class"
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+    echo "Missing command"
+    exit 1
+fi
+
 THE_BASH_SOURCE=$BASH_SOURCE
 READ_LINK=$(readlink $THE_BASH_SOURCE)
 if [ "$READ_LINK" != "" ]; then
