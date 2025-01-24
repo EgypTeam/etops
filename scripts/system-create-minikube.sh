15fae719a1a1985e0a70a913571d74965e939211
Pedro Ferreira <pedro.ferreira@egypteam.com>
2025-01-24 10:13:40 -0300

    Update

--- scripts/system-create-minikube.sh
+++ scripts/system-create-minikube.sh
@@ -6,13 +6,14 @@ IPADDRESS=$(hostname -I | cut -f 1 -d" ")
 ALLIPADDRESSES=$(hostname --ip-address | tr " " ",")
 
 minikube start \
+    --driver docker \
     --profile etops \
     --listen-address 0.0.0.0 \
     --mount-string="/c:/c" \
     --mount  \
     --apiserver-names=$HOSTNAME \
     --apiserver-ips=$IPADDRESS \
-    --static-ip=192.168.58.2
+    --static-ip=192.168.49.2
 
 # /etc/kubernetes/manifests/kube-apiserver.yaml
 # --apiserver-service-node-port-range=1-65535
