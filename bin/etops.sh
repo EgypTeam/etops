d9bc1c1259f97e7395a063e6e5fbb7ecabd4f195
Pedro Ferreira <pedro.ferreira@egypteam.com>
2025-01-29 09:32:43 -0300

    Update

--- bin/etops.sh
+++ bin/etops.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 THECOMMAND=$1
+PONE=
+PTWO=
+PTWOREST=
+PTHREEREST=
 
 if [ "$THECOMMAND" == "" ]; then
     #echo "Missing command class"
     #exit 1
     THECOMMAND=ui
+    PONE=ui
+    PTWO=
+    PTWOREST=
+    PHREEREST=
+else
+    PONE=$1
+    PTWO=$2
+    PTWOREST=${@:2}
+    PTHREEREST=${@:3}
 fi
 
 THE_BASH_SOURCE=$BASH_SOURCE
@@ -18,8 +31,8 @@ BINDIR=$(dirname $THE_BASH_SOURCE)
 BASEDIR=$(dirname $BINDIR)
 SCRIPTSDIR="$BASEDIR/scripts"
 
-if [ -f $SCRIPTSDIR/$1.sh ]; then
-    $SCRIPTSDIR/$1.sh ${@:2}
-elif [ -f $SCRIPTSDIR/$1-$2.sh ]; then
-    $SCRIPTSDIR/$1-$2.sh ${@:3}
+if [ -f $SCRIPTSDIR/$PONE.sh ]; then
+    $SCRIPTSDIR/$PONE.sh $PTWOREST
+elif [ -f $SCRIPTSDIR/$PONE-$PTWO.sh ]; then
+    $SCRIPTSDIR/$1-$2.sh $PTHREEREST
 fi
