d3751cb1c6d6d6d7ffdcb21e387eaaf665a1997c
Pedro Ferreira <pedro.ferreira@egypteam.com>
2025-02-08 13:22:08 -0300

    Update

--- bin/etops.sh
+++ bin/etops.sh
@@ -23,6 +23,10 @@ fi
 
 THE_BASH_SOURCE=$BASH_SOURCE
 READ_LINK=$(readlink -f $THE_BASH_SOURCE)
+ETOPS_COMMAND=$(basename $BASH_SOURCE)
+ETOPS_COMMAND_ABSOLUTE_PATH=$(realpath $READ_LINK)
+#echo $ETOPS_COMMAND
+#echo $ETOPS_COMMAND_ABSOLUTE_PATH
 if [ "$READ_LINK" != "" ]; then
     THE_BASH_SOURCE=$READ_LINK
 fi
@@ -31,8 +35,14 @@ BINDIR=$(dirname $THE_BASH_SOURCE)
 BASEDIR=$(dirname $BINDIR)
 SCRIPTSDIR="$BASEDIR/scripts"
 
+ALLPARMS=$@
+
 if [ -f $SCRIPTSDIR/$PONE.sh ]; then
-    $SCRIPTSDIR/$PONE.sh $PTWOREST
+    set --
+    source $SCRIPTSDIR/$PONE.sh $PTWOREST
+    set -- $ALLPARMS
 elif [ -f $SCRIPTSDIR/$PONE-$PTWO.sh ]; then
-    $SCRIPTSDIR/$1-$2.sh $PTHREEREST
+    set --
+    source $SCRIPTSDIR/$PONE-$PTWO.sh $PTHREEREST
+    set -- $ALLPARMS
 fi
