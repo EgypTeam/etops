5872308792615716727a65c4075257a96404aff8
Pedro Ferreira <pedro.ferreira@egypteam.com>
2025-03-29 15:42:02 -0300

    Update

---
+++ scripts/init-private.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git submodule add git@github.com:EgypTeam/etops-descriptors.git descriptors
+git submodule add git@github.com:EgypTeam/etops-haproxy.git haproxy
+git submodule add git@github.com:EgypTeam/etops-confscripts-after-delete.git confscripts-after-delete
+git submodule add git@github.com:EgypTeam/etops-confscripts-before-create.git confscripts-before-create
