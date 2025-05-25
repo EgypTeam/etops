35d7c2a4730ff12aeda21bd59225b771aee20b57
Pedro Ferreira <pedro.ferreira@egypteam.com>
2025-05-25 05:03:29 -0300

    feature(update-references)

---
+++ scripts/progen-java-library-jar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_NAME=$1
+
+if [ -f "$1" ] || [ -d "" ]; then
+  echo "File / Directory exists"
+  exit 1
+fi
+
+ORIGINALDIR=$PWD
+
+git clone git@github.com:NiloTemplates/nilo-java-library-jar.git "$PROJECT_NAME"
+rm -rf $PROJECT_NAME/.git
+cd $PROJECT_NAME;
+
---
+++ scripts/progen-java-swing-app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT_NAME=$1
+
+if [ -f "$1" ] || [ -d "" ]; then
+  echo "File / Directory exists"
+  exit 1
+fi
+
+ORIGINALDIR=$PWD
+
+git clone git@github.com:NiloTemplates/nilo-java-swing-app.git "$PROJECT_NAME"
+rm -rf $PROJECT_NAME/.git
+cd $PROJECT_NAME;
+
+
