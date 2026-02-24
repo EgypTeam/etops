8ee5377a96461d53a1ec40477f74040dc29ca8e1
Pedro Ferreira <pedrofmj@gmail.com>
2026-02-24 08:35:43 +0000

    update

---
+++ scripts/system-create2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#minikube start --listen-address 0.0.0.0 --apiserver-names=satellite-silverstar --apiserver-ips=192.168.68.145 --mount-string="/c:/c" --mount
+
+IPADDRESS=$(hostname -I | cut -f 1 -d" ")
+ALLIPADDRESSES=$(hostname --ip-address | tr " " ",")
+
+minikube start \
+    --driver docker \
+    --profile quantum \
+    --listen-address 0.0.0.0 \
+    --mount-string="/c:/c" \
+    --mount  \
+    --apiserver-names=$HOSTNAME \
+    --apiserver-ips=$IPADDRESS \
+    --static-ip=192.168.58.2 \
+    --extra-config=apiserver.service-node-port-range=1-65535
+
+minikube addons enable ingress --profile quantum
+
+# /etc/kubernetes/manifests/kube-apiserver.yaml
+# --apiserver-service-node-port-range=1-65535
+
+
